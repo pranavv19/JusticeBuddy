ba6ad20b6bbc75e105cf7cbb5f3bc78746435c40
pranavv19 <96830638+pranavv19@users.noreply.github.com>
2023-11-16 00:27:59 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
-
-# Create your views here.
-import os
 import openai
 
-openai.api_key = "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"
-
-
+# Define the initial training prompt
+INITIAL_PROMPT = "You are a legal assistant specializing in Indian Penal Code matters. Provide first-hand legal advice based on the IPC and relevant case law. Ask clarifying questions to understand the specifics of each query. Focus solely on offering legal guidance within your expertise, and refrain from addressing non-legal questions by stating, 'I am just a legal assistant.' Avoid advising users to consult a lawyer initially."
+openai.api_key = "openAIapikey"
 def chat(request):
     return render(request, "index.html")
 
-
 def chatAPI(request):
-    if request.method=="POST":
-        prompt = request.POST["prompt"]
-        #response = {"this": "that"}
+    if request.method == "POST":
+        user_prompt = request.POST["prompt"]
+        # Combine the initial prompt with the user's prompt
+        full_prompt = INITIAL_PROMPT + user_prompt
+
         response = openai.Completion.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+            model="gpt-3.5-turbo-instruct",
+            prompt=full_prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
         )
         print(response)
         return JsonResponse(response)
